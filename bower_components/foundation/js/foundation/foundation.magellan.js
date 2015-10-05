2da34d9fe81af68fbb342e330b7c85b5094e1877
Tim Surkov <timmy.sur234@gmail.com>
2015-10-06 00:48:25 +0300

    Added some basic structure to portfolio page

--- bower_components/foundation/js/foundation/foundation.magellan.js
+++ bower_components/foundation/js/foundation/foundation.magellan.js
@@ -10,10 +10,10 @@
       active_class : 'active',
       threshold : 0, // pixels from the top of the expedition for it to become fixes
       destination_threshold : 20, // pixels from the top of destination for it to be considered active
-      throttle_delay : 30, // calculation throttling to increase framerate
+      throttle_delay : 50, // calculation throttling to increase framerate
       fixed_top : 0, // top distance in pixels assigend to the fixed element on scroll
       offset_by_height : true,  // whether to offset the destination by the expedition height. Usually you want this to be true, unless your expedition is on the side.
-      duration : 700, // animation duration time
+      duration : 300, // animation duration time
       easing : 'swing' // animation easing
     },
 
